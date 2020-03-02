6a5bca92851d879a2099f3ac080ba70f37fdd5c0
heyyyjonardon <57803702+heyyyjonardon@users.noreply.github.com>
2020-03-03 00:30:14 +0530

    Create chapter_crawler.py

---
+++ chapter_crawler.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from selenium import webdriver
+
+x = webdriver.Firefox()
+
+def next_chapter():
+    x.get('https://www.learncbse.in/cbse-notes-class-11-chemistry/')
+    xpath = "/html/body/div[1]/div[1]/div/main/article/div/div[2]/ul/li[{}]/a".format(int(input_field.get()))
+    chap = x.find_element_by_xpath(xpath)
+    chap.click()
+    
+#
+
+widget = Tk()
+
+var = StringVar()
+text = Label(widget,textvariable=var)
+var.set('Enter next chapter to jump to: ')
+text.grid()
+
+input_field = Entry(widget)
+input_field.grid()
+
+
+jump = Button(widget,text="JUMP",command=next_chapter)
+jump.grid()
+
+
+widget.title('Chapter Crawler')
+
+widget.geometry('200x100')
+
+widget.mainloop()
