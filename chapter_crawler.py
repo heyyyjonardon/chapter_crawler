f6ca8afa149d1d0310386457d181c04c4c7b3a16
Jonardon Hazarika <57803702+heyyyjonardon@users.noreply.github.com>
2020-03-09 20:51:04 +0530

    Update chapter_crawler.py

--- chapter_crawler.py
+++ chapter_crawler.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from selenium import webdriver
 
-x = webdriver.Firefox()
+x = webdriver.Chrome('c:/chromedriver.exe')
 
 def next_chapter():
     x.get('https://www.learncbse.in/cbse-notes-class-11-chemistry/')
@@ -25,7 +25,12 @@ input_field.grid()
 jump = Button(widget,text="JUMP",command=next_chapter)
 jump.grid()
 
-quit_button = Button(widget,text="Hmm..QUIT!",command=widget.quit)
+
+def close():
+    widget.destroy()
+    x.close()
+
+quit_button = Button(widget,text="Hmm..QUIT!",command=close)
 quit_button.grid()
 
 widget.title('Chapter Crawler')
